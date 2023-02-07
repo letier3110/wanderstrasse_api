5a2c3faf5b47e366817fd99b7c20496ab32abcaf
letier3110 <letier3110@gmail.com>
2023-02-07 21:41:43 +0400

    feat: added external widget parse

--- tg.ts
+++ tg.ts
@@ -3,6 +3,7 @@ let lastIndex = 4449
 interface IPost {
   text: string
   image: string
+  widget: string
 }
 
 let buffer: IPost[] = []
@@ -13,18 +14,24 @@ const endingPhrase = `<meta property="twitter:title"`
 const initialImagePhrase = `<meta property="og:image" content="`
 const endingImagePhrase = `<meta property="og:site_name" content="Telegram">`
 
+const initialWidgetPhrase = `<a class="tgme_widget_message_link_preview"`
+const endingWidgetPhrase = `<div class="tgme_widget_message_footer js-message_footer">`
+
 const getInitial = async () => {
   let isValid = await checkIfValid(lastIndex)
+  const embedText = await getEmbed(lastIndex)
 
   if (isValid) {
     // console.log('valid')
     const extractedText = extractText(isValid)
     const extractedImage = extractImage(isValid)
+    const extractedWidget = extractWidget(embedText)
     // console.log(extractedText)
     // console.log(extractedImage)
     buffer.push({
       text: extractedText,
-      image: extractedImage
+      image: extractedImage,
+      widget: extractedWidget
     })
   } else {
     // console.log('not valid')
@@ -34,15 +41,18 @@ const getInitial = async () => {
   while (isValid) {
     lastIndex += 1
     isValid = await checkIfValid(lastIndex)
+    const embedText = await getEmbed(lastIndex)
     if (isValid) {
       // console.log('valid cycle')
       const extractedText = extractText(isValid)
       const extractedImage = extractImage(isValid)
+      const extractedWidget = extractWidget(embedText)
       // console.log(extractedText)
       // console.log(extractedImage)
       buffer.push({
         text: extractedText,
-        image: extractedImage
+        image: extractedImage,
+        widget: extractedWidget
       })
     } else {
       // console.log('not valid cycle')
@@ -68,6 +78,16 @@ const extractImage = (text: string) => {
   return sliceJS
 }
 
+const extractWidget = (text: string) => {
+  const beginingIndex = text.indexOf(initialWidgetPhrase)
+  const endingIndex = text.indexOf(endingWidgetPhrase, beginingIndex)
+  if(beginingIndex < 0 || endingIndex < 0) {
+    return ''
+  }
+  const sliceJS = text.slice(beginingIndex, endingIndex)
+  return sliceJS
+}
+
 const checkIfValid = async (id: number) => {
   const url = `https://t.me/wanderstrasse/${id}`
   const errorPhrase = `If you have <strong>Telegram</strong>, you can view post <br>and join <strong dir="auto">`
@@ -77,6 +97,14 @@ const checkIfValid = async (id: number) => {
   if (errorIndex >= 0) {
     return null
   }
+  return requestText
+}
+
+const getEmbed = async (id: number) => {
+  const url = `https://t.me/wanderstrasse/${id}?embed=1&mode=tme`
+  // const errorPhrase = `If you have <strong>Telegram</strong>, you can view post <br>and join <strong dir="auto">`
+  const request = await fetch(url)
+  const requestText = await request.text()
   return requestText
 }
 
